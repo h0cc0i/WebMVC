0abe385597a188107a222fb1f01b7bed23b7c2ac
h0cc0i <chunglaithanh@gmail.com>
2017-01-14 01:30:41 +0900

    meo hieu

---
+++ WebMVC.COMMON/CommonConstans.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebMVC.COMMON
+{
+    public static class CommonConstans
+    {
+        public static string USER_SESSION = "USER_SESSION";
+        public const int PageSize = 25;
+    }
+}
---
+++ WebMVC.ENTITIES/CrudModel/CrudModelDMQuyenQuanTri.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebMVC.ENTITIES.CrudModel
+{
+    [Serializable]
+    public class CrudModelDMQuyenQuanTri
+    {
+        public CrudModelDMQuyenQuanTri()
+        {
+            this.CrudModelQuanTriViens = new HashSet<CrudModelQuanTriVien>();
+        }
+
+        [Display(Name = "Mã quản trị viên")]
+        public int MaQuyenQuanTri { get; set; }
+
+        [Display(Name = "Tên quyền quản trị")]
+        public string TenQuyenQuanTri { get; set; }
+
+        public virtual ICollection<CrudModelQuanTriVien> CrudModelQuanTriViens
+        {
+            get; set;
+        }
+    }
+}
